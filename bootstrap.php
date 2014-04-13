09ebbc1387ed6807c85ea96dc7dca79c9bb62720
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2014-04-13 00:33:57 -0300

    annotation alias improve
    annotation alias improve, mysql utf8 and removed audit.

--- bootstrap.php
+++ bootstrap.php
@@ -1,34 +1,35 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use \Doctrine\ORM\Tools\Setup;
-use \Doctrine\ORM\EntityManager;
-use \Doctrine\Common\EventManager;
-use \SimpleThings\EntityAudit\AuditConfiguration;
-use \SimpleThings\EntityAudit\AuditManager;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\EventManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 define('PASS_SALT', 'ChangeIt,Please!');
 
 $isDev = true;
-$paths = array(__DIR__ . '/lib/Application/Model');
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDev);
+$paths = array(__DIR__ . '/src/Application/Model');
+$driver = new AnnotationDriver(new AnnotationReader(), $paths);
+AnnotationRegistry::registerLoader('class_exists');
+$config = Setup::createConfiguration($isDev);
+$config->setMetadataDriverImpl($driver);
 $config->setAutoGenerateProxyClasses($isDev);
 $config->setProxyDir(__DIR__ . '/tmp');
+
 
 $conn = array(
     'host'     => 'localhost',
     'user'     => 'root',
     'password' => '',
     'dbname'   => '',
-    'driver'   => 'pdo_mysql'
+    'driver'   => 'pdo_mysql',
+    'charset' => 'UTF8',
+    'driverOptions' => array(
+       'SET NAMES utf8;'
+    )
 );
-
-$auditconfig = new AuditConfiguration();
-$auditconfig->setAuditedEntityClasses(array(
-    //'Application\Model\AuditableModelHere'
-));
-$evm = new EventManager();
-$auditManager = new AuditManager($auditconfig);
-$auditManager->registerEvents($evm);
 
 $entityManager = EntityManager::create($conn, $config, $evm);
